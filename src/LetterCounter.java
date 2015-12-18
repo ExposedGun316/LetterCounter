d37d7f5d598287ca10c8298f124366084150b9f4
ExposedGun316 <exposedgun316@live.co.uk>
2015-12-18 10:57:21 +0000

    Added in counter

--- src/LetterCounter.java
+++ src/LetterCounter.java
@@ -7,5 +7,13 @@ import java.util.Scanner;
 public class LetterCounter {
     public static void main(String[] args){
 
+        String counter;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Please enter a word:");
+        counter =  userInput.next();
+
+        System.out.println(counter.length());
+
     }
 }
